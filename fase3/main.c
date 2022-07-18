0db2cb5a4d92ff66eca251102e879f63958cab7d
Eduardo Santos <santos.eduardothomaz@gmail.com>
2022-07-18 10:06:42 -0300

    fix(fase3/thread-test): fixed and moved to new folder

--- fase3/main.c
+++ fase3/main.c
@@ -3,15 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
-#ifdef _WIN32
-#include <Windows.h>
-#else
 #include <unistd.h>
-#endif
-
-#define true 1
-#define false 0
 
 const int SECOND = 1000;
 
